f01b75b1bf4062dae3a8e2d09471bdb411499854
CassandraDM <cassandra.delmas-marchiset@mail-esd.com>
2025-01-06 09:13:01 +0100

    on revient a quelque chose qui marche

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import "./HomePage.css";
-import "./createClient";
+import { createClient } from "@/prismicio";
 import { Metadata } from "next";
 import { SliceZone } from "@prismicio/react";
 import * as prismic from "@prismicio/client";
 import { components } from "../slices";
-import { useState } from "react";
 
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
@@ -20,97 +19,31 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-export default function Index() {
-  const [formData, setFormData] = useState({
-    firstname: "",
-    lastname: "",
-    email: "",
-    message: "",
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const response = await fetch("/api/submitForm", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-
-    if (response.ok) {
-      alert("Form submitted successfully");
-      setFormData({
-        firstname: "",
-        lastname: "",
-        email: "",
-        message: "",
-      });
-    } else {
-      alert("Error submitting form");
-    }
-  };
+export default async function Index() {
+  // The client queries content from the Prismic API
+  const client = createClient();
+  const home = await client.getByUID("page", "home");
 
   return (
     <div className="container">
       <SliceZone slices={home.data.slices} components={components} />
-      <form className="contact-form" onSubmit={handleSubmit}>
+      <form className="contact-form">
         <h2>Contact Us</h2>
         <div className="form-group">
           <label htmlFor="firstname">First Name:</label>
-          <input
-            type="text"
-            id="firstname"
-            name="firstname"
-            placeholder="Your first name"
-            value={formData.firstname}
-            required
-            onChange={handleChange}
-          />
+          <input type="text" id="firstname" name="firstname" required />
         </div>
         <div className="form-group">
-          <label htmlFor="lastname">Last Name:</label>
-          <input
-            type="text"
-            id="lastname"
-            name="lastname"
-            placeholder="Your last name"
-            value={formData.lastname}
-            required
-            onChange={handleChange}
-          />
+          <label htmlFor="lastname">LastName:</label>
+          <input type="text" id="lastname" name="lastname" required />
         </div>
         <div className="form-group">
           <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Your email address"
-            value={formData.email}
-            required
-            onChange={handleChange}
-          />
+          <input type="email" id="email" name="email" required />
         </div>
         <div className="form-group">
           <label htmlFor="message">Message:</label>
-          <textarea
-            id="message"
-            name="message"
-            placeholder="Your message"
-            value={formData.message}
-            required
-            onChange={handleChange}
-          />
+          <textarea id="message" name="message" required></textarea>
         </div>
         <button type="submit">Submit</button>
       </form>
--- src/app/prismicClient.ts
+++
@@ -1,9 +0,0 @@
-import * as prismic from "@prismicio/client";
-
-export const createClient = () => {
-  const client = prismic.createClient("your-repo-name", {
-    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
-  });
-
-  return client;
-};
